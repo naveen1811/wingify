0338c265274ac0aab79c674d384de75af7c2e8c4
naveen1811 <naveen666agarwal@gmail.com>
2016-05-27 15:03:50 +0530

    1

--- wingify/settings.py
+++ wingify/settings.py
@@ -89,7 +89,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
