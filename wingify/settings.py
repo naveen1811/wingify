4ac36fe27ee7f1edcd23f38ef43f62ae995b4fa3
naveen1811 <naveen666agarwal@gmail.com>
2016-05-27 18:59:16 +0530

    14

--- wingify/settings.py
+++ wingify/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +56,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'wingify.urls'
 
-WSGI_APPLICATION = 'wingify.wsgi.application'
+WSGI_APPLICATION = 'wingify.wsgi.applicaPROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))tion'
 
 
 # Database
@@ -94,13 +94,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-ADMIN_MEDIA_PREFIX = '/static/admin/' 
 # Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = (
-#    os.path.join(PROJECT_ROOT, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
